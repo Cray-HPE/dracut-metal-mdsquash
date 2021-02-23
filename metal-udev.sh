88b71f7901f3da4a462180257526655258baf548
Russell Bunch <rbunch@cray.com>
2021-02-23 17:46:28 -0600

    Remove unset because it breaks the the type check

--- metal-udev.sh
+++ metal-udev.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 # metal-udev.sh ; copies all new udev rules into the overlayFS
 
-# die if we have anything unset; don't copy files into undesirable places.
-set -u
-
-type metal_die > /dev/null 2>&1 || . /lib/metal-md-lib.sh
+type metal_die >/dev/null 2>&1 || . /lib/metal-md-lib.sh
 
 made=/etc/udev/rules.d/
-live=/run/initramfs/overlayfs/${live_dir}/$(_overlayFS_path_spec)/etc/udev/rules.d/
+live=/run/initramfs/overlayfs/${live_dir}/$(_overlayFS_path_spec)/etc/udev/rules.d
 
 ls -l $live
-cp -pv $made* $live || metal_die 'FATAL: udev rules did not provision from dracut!! Beware of udev problems.'
+mkdir -pv $live
+cp -pv $made* $live/ || metal_die 'FATAL: udev rules did not provision from dracut!! Beware of udev problems.'
 ls -l $live
