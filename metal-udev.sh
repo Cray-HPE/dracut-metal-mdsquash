1dbd44debc4c207dfd2f30fcb4ee4e337824560b
Russell Bunch <rbunch@cray.com>
2021-02-24 10:38:20 -0600

    CASMINST-1554 copy into the active overlay not the store

--- metal-udev.sh
+++ metal-udev.sh
@@ -4,8 +4,8 @@
 type metal_die >/dev/null 2>&1 || . /lib/metal-md-lib.sh
 
 made=/etc/udev/rules.d/
-live=/run/initramfs/overlayfs/${live_dir}/$(_overlayFS_path_spec)/etc/udev/rules.d
+live=/run/overlayfs/etc/udev/rules.d
 
-ls -l $live 2>/dev/null || mkdir -pv $live
+mkdir -pv $live
 cp -pv $made* $live/ || metal_die 'FATAL: udev rules did not provision from dracut!! Beware of udev problems.'
 ls -l $live
