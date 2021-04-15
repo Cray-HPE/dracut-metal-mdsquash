11c2b47f6ced420020db3fc3c4c755950aca7694
Russell Bunch <rbunch@cray.com>
2021-04-15 11:17:31 -0500

    CASMINST-2002 ifname before NICs are UPPED

--- 90metalmdsquash/metal-udev.sh
+++ 90metalmdsquash/metal-udev.sh
@@ -11,7 +11,8 @@ ls -l $live
 # We want our ifname rules to come after any drivers, since these denote the NIC names 
 # the NCNs are intentionally providing. All other NICs are aliens, and should not be
 # considered without filing a bug for investigation of origin.
+# ifname rules should be applied at the last moment, that is before we UP the NICs in 90-net.rules.
 ifname_rules=$live/*ifname.rules
-mv $ifname_rules $live/98-ifname.rules
+mv $ifname_rules $live/90-ifname.rules
 ls -l $live
 
