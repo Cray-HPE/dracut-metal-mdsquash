8a8a7c53f36e5ce877137b980f3d20d2b868c394
Russell Bunch <doomslayer@hpe.com>
2022-06-21 13:07:00 -0500

    MTL-1288 Fix Race Condition and Debug Mode (#36)
    * MTL-1288 Fix Race Conditions and Debug Mode
    
    This adds support for preventing race conditions.
    - Add new paved function to report whether the pave has completed or not.
    This new function will return 0 or 1 if the wipe is in-progress or has
    completed, thus allowing dracut modules to wait on the pave before
    starting their functions.
    - Set wait_for_dev locks to wait for the OS disks to ensure dracut
    waits for this module to finish, preventing dmsquash-live from
    racing ahead when SQFSRAID is detected. Also remove the previously
    added labeling code that was working around the race condition,
    it only worked on the first deployment. Subsequent reboots were
    susceptible to the race condition. This fix mends the race
    condition on any boot.
    
    This also fixes missing or broken statements for enabling debug mode.
    Metal.debug was not honoring metal.debug=1 in some cases. Ensure all
    files (except module-setup) honor metal.debug.
    
    Also create & use a new disks_exist function, this will ensure we wait
    for systems with /dev/sd or /dev/nvme block devices (a mix or pure).
    Otherwise it was possible to wait forever on systems with only
    NVME, meaning dracut only worked with mixed systems (SD + NVME, or SD
    only).
    
    Fix broken warn statements, ensure quotes wrap metal_die and warn
    statements to prevent incorrect warnings.
    
    Make disk boots safer by moving the empty metal.server case to its
    own case that invokes metal-md-scan. This case is for disk boots,
    and only in this case will the mdscan function be used. This move
    helps separate context, when metal.server is empty then none of the
    disk creation calls are made. When metal.server is empty we can
    assume that disks were already created. This is safer, since none
    of the disk creation code will load in a disk boot.
    
    Add/update licenses.
    
    Add smarter fstab handling; merge old and new if different on reboot,
    and after validating they exist.
    
    Add a reset to metal_die that when activated will reset the system.
    
    * MTL-1497 Add Shellcheck

--- 90metalmdsquash/metal-update-fstab.sh
+++ 90metalmdsquash/metal-update-fstab.sh
@@ -1,14 +1,76 @@
 #!/bin/bash
-set -x
-type metal_die > /dev/null 2>&1 || . /lib/metal-lib.sh
+#
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+[ "${metal_debug:-0}" = 0 ] || set -x
+command -v metal_die > /dev/null 2>&1 || . /lib/metal-lib.sh
 
 fstab_metal_new=$metal_fstab
 fstab_metal_old=/sysroot$metal_fstab
+fstab_metal_temp=${metal_fstab}.merged
 
-if [ -f "$fstab_metal_old" ]; then
-    mkdir -pv "$(mount -a -f -v -T $fstab_metal_new | awk '{print $1}' | tr -s '\n' ' ')"
-    mount -a -v -T "$fstab_metal_old"
-else
-    # If a new FSTab exists, this copies it regardless if there is no diff.
-    [ -f "$fstab_metal_new" ] && cp -v "$fstab_metal_new" "$fstab_metal_old"
+old_error=0
+new_error=0
+
+# If a new FSTab exists, this copies it regardless if there is no diff.
+if [ -f $fstab_metal_new ]; then
+    
+    # If no prior fstab exists, copy the new one into place.
+    if [ ! -f $fstab_metal_old ]; then
+        cp -v "$fstab_metal_new" "$fstab_metal_old"
+
+    # If a prior fstab exists, merge it and verify all the labels exist before copying it into place.
+    elif diff -q $fstab_metal_old $fstab_metal_new ; then
+
+        # Make new fstab file, remove commented out lines.
+        cat "$fstab_metal_old" "$fstab_metal_new" | sort -u | grep -v '^#' >"$fstab_metal_temp"
+
+        # Verify the old fstab file was valid.
+        for label in $(grep LABEL $fstab_metal_old | awk '{print $1}' | awk -F '=' '{print $NF}'); do 
+            if ! blkid -L $label >/dev/null; then
+                echo >&2 'Old fstab is invalid.'
+                old_error=1
+                break
+            fi
+        done
+
+        # Verify the new fstab file is valid.
+        for label in $(grep LABEL $fstab_metal_new | awk '{print $1}' | awk -F '=' '{print $NF}'); do 
+            if ! blkid -L $label >/dev/null; then
+                echo >&2 'New fstab is invalid.'
+                
+                # The new fstab contains new partitions that do not exist.
+                new_error=1
+                break
+            fi
+        done
+        
+        # If no errors, copy the merged fstab into place. Otherwise fail with a fatal error for inspection.
+        if [ "$old_error" = 0 ] && [ "$new_error" = 0 ]; then
+            # All the labels in our merged fstab exist.
+            cp -v "$fstab_metal_temp" "$fstab_metal_old"
+        else
+            metal_die "FATAL FSTAB ERROR: One or more expected partitions do not exist. Please verify contents of $fstab_metal_old $fstab_metal_new and $fstab_metal_temp."
+        fi
+    fi
 fi
