e748c309168536f497b7030186c1a7c385f1d52e
John Heemstra <heemstra@users.noreply.github.com>
2023-09-11 13:04:29 -0400

    MTL-2272: remove xfs options; use defaults (#61)
    It was discovered that allocsize, in particular, was causing
    problems during IMS image builds. The db files for the 'man'
    package were being forced to a size that was much larger than
    what was needed, both wasting space and causing build times to
    be significantly increased.

--- 90metalmdsquash/metal-lib.sh
+++ 90metalmdsquash/metal-lib.sh
@@ -128,7 +128,7 @@ export metal_fstab=/etc/fstab.metal
 # constant: metal_fsopts_xfs
 #
 # COMMA-DELIMITED-LIST of fsopts for XFS
-export metal_fsopts_xfs=noatime,largeio,inode64,swalloc,allocsize=131072k
+export metal_fsopts_xfs=
 
 ##############################################################################
 # constant: metal_disk_small
